ec293244bb6eb7e3aa17048de1ba4873ce5835ad
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-07-28 16:40:04 +0545

    setup backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//database configuraion
+const db = require("./config/key").mongoURI;
+
+//connect to the mongodb
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("database connectef succefully"))
+  .catch(err => console / log(err));
+
+//use routes here
+app.get("api/users", users);
+app.get("api/profile", profile);
+app.get("api/posts", posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server running localhost:${port}`));
