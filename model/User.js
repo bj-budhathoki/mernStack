1fc94990d613f595514c6a089dee31bd908dab9b
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-07-28 17:29:55 +0545

    user schema set up

--- model/User.js
+++ model/User.js
@@ -2,4 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //creating the user schema
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
