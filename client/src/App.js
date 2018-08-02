d2f3927b809a5acea75f4ec1356b54a184be0ae3
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-08-02 22:25:45 +0545

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+//router
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //components
 import Navbar from "./components/layout/Navbar";
@@ -8,11 +10,13 @@ import Landing from "./components/layout/Landing";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
