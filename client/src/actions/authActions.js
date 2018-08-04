858813c847aaa98c34dc491192baabe546d67aaf
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-08-04 15:38:42 +0545

    token setup

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+//Register user
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
@@ -10,4 +13,34 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+//login
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save to localStorage
+      const { token } = res.data;
+      //set token to localstorege
+      localStorage.setItem("jwttoken", token);
+      //set teken to auth header
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      //set the current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set the current user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
