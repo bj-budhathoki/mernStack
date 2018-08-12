b149423b31b3713873c6333bcb844e51e9661812
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-08-12 11:56:51 +0545

    adding spinner

--- client/src/components/dashboard/dashBoard.js
+++ client/src/components/dashboard/dashBoard.js
@@ -3,20 +3,46 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 //actions
 import { getCurrentProfile } from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
 class dashboard extends Component {
   componentDidMount = () => {
     this.props.getCurrentProfile();
   };
 
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+    let dashBoardContent;
+    if (profile === null || loading) {
+      dashBoardContent = <Spinner />;
+    } else {
+      dashBoardContent = <h1>Hello</h1>;
+    }
     return (
-      <div>
-        <h1>dashBoard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashBoardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+dashboard.propTypes = {
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired
+};
+const mapStsteToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
 export default connect(
-  null,
+  mapStsteToProps,
   { getCurrentProfile }
 )(dashboard);
